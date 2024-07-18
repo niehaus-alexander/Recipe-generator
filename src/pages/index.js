4f2e1efc363f6abafc2aca23b316fa9cf8f571b2
Alexander <alexander.niehaus@gmx.net>
2024-07-18 13:58:44 +0200

    add navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <html data-theme="cupcake">
-      <div data-theme="cupcake">
-        <h1>Hello World</h1>
-        <button class="btn btn-primary">Button</button>
-      </div>
-    </html>
-  );
+  return <></>;
 }
---
+++ src/pages/recipes.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+const recipes = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default recipes;
