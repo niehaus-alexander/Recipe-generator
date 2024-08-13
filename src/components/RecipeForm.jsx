8f12a21f88124234fb93ef189d1499ef53322731
Alexander <alexander.niehaus@gmx.net>
2024-08-13 14:42:32 +0200

    add form layout

---
+++ src/components/RecipeForm.jsx
@@ -0,0 +1,37 @@
+const RecipeForm = () => {
+  return (
+    <form className="border border-solid flex flex-col justify-center items-center gap-2 m-6 p-6 rounded-md">
+      <legend className="label">Add a new recipe:</legend>
+      <input
+        type="text"
+        placeholder="Recipe Name"
+        className="input input-bordered input-primary w-full max-w-xs"
+        required
+      />
+
+      <input
+        type="text"
+        placeholder="Cuisine"
+        className="input input-bordered input-primary w-full max-w-xs"
+      />
+      <div className="border border-solid border-primary rounded-[2rem] p-4 w-full max-w-xs">
+        <label>Difficulty</label>
+        <input
+          type="range"
+          min={0}
+          max="100"
+          className="range range-primary w-full max-w-xs"
+          step="50"
+        />
+
+        <div className="flex w-full max-w-xs justify-between px-2 text-xs">
+          <span>easy</span>
+          <span>moderat</span>
+          <span>hard</span>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default RecipeForm;
