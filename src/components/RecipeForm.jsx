857013ac5fcafb3acf1ad26be61873751dfbca34
Alexander <alexander.niehaus@gmx.net>
2024-08-13 14:48:01 +0200

    adjust form design

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -1,7 +1,9 @@
 const RecipeForm = () => {
   return (
     <form className="border border-solid flex flex-col justify-center items-center gap-2 m-6 p-6 rounded-md">
-      <legend className="label">Add a new recipe:</legend>
+      <legend className="text-neutral text-lg font-semibold">
+        Add a new recipe:
+      </legend>
       <input
         type="text"
         placeholder="Recipe Name"
@@ -15,7 +17,7 @@ const RecipeForm = () => {
         className="input input-bordered input-primary w-full max-w-xs"
       />
       <div className="border border-solid border-primary rounded-[2rem] p-4 w-full max-w-xs">
-        <label>Difficulty</label>
+        <label className="block mb-2">Difficulty</label>
         <input
           type="range"
           min={0}
