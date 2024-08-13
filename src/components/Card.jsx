4473f0fafd2c2bb8b6d510a79460537ce45346cc
Alexander <alexander.niehaus@gmx.net>
2024-08-13 13:41:45 +0200

    switch to new card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,19 @@
-import Image from "next/image";
-
 const Card = ({ data }) => {
   return (
-    <div className="container card card-side bg-base-100 shadow-xl m-12 max-w-xs sm:max-w-sm md:max-w-md">
-      <figure>
-        <Image
-          src="https://images.unsplash.com/photo-1503810473512-f64b56827964?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZvb2R8ZW58MHx8MHx8fDI%3D"
-          alt="Food"
-          height={150}
-          width={150}
-          className="rounded-lg"
-        />
-      </figure>
+    <div className="card bg-primary text-primary-content w-96 m-6">
       <div className="card-body">
-        <h2 className="card-title break-words">{data.name}</h2>
-        <p>Cuisine: {data.cuisine}</p>
-        <p>Time: {data.timeInMinutes} min.</p>
-        <p>Difficulty: {data.difficulty}</p>
+        <h2 className="card-title">{data.name}</h2>
+        <p>
+          <span className="font-semibold">Time:</span> {data.timeInMinutes}
+        </p>
+        <p>
+          <span className="font-semibold">Cuisine:</span> {data.cuisine}
+        </p>
+        <p>
+          <span className="font-semibold">Difficulty:</span> {data.difficulty}
+        </p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Details</button>
+          <button className="btn">Details</button>
         </div>
       </div>
     </div>
