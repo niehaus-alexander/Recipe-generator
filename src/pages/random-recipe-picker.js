a63e63f68424951423e9d65fe3fbe571fbf9f65a
Alexander <alexander.niehaus@gmx.net>
2024-08-29 17:08:07 +0200

    add favorites randomizer

--- src/pages/random-recipe-picker.js
+++ src/pages/random-recipe-picker.js
@@ -1,4 +1,19 @@
-export default function RandomRecipePickerPage() {
+import { useState } from "react";
+import Card from "@/components/Card";
+
+export default function RandomRecipePickerPage({
+  favoriteRecipes,
+  onToggleFavorites,
+}) {
+  const [randomFavoritesRecipe, setRandomFavoritesRecipe] = useState(null);
+
+  function handlePickRandomFavoritesRecipe() {
+    if (favoriteRecipes.length > 0) {
+      const randomIndex = Math.floor(Math.random() * favoriteRecipes.length);
+      setRandomFavoritesRecipe(favoriteRecipes[randomIndex]);
+    }
+  }
+
   return (
     <div className="w-full">
       <div className="max-w-screen-lg mx-auto">
@@ -16,8 +31,29 @@ export default function RandomRecipePickerPage() {
           </a>
           .
         </h2>
-        <div className="w-full p-6">
-          <h2></h2>
+        <div className="w-full p-6 flex justify-center items-center flex-col mb-10">
+          <h2 className="text-center font-semibold text-3xl mb-10">
+            Favorites
+          </h2>
+          <button
+            onClick={handlePickRandomFavoritesRecipe}
+            className="btn btn-secondary mb-6"
+          >
+            Pick random recipe!
+          </button>
+          {randomFavoritesRecipe && (
+            <Card
+              onToggleFavorites={onToggleFavorites}
+              data={randomFavoritesRecipe}
+              favoriteRecipes={favoriteRecipes}
+            />
+          )}
+        </div>
+        <div className="w-full p-6 flex justify-center items-center flex-col ">
+          <h2 className="text-center font-semibold text-3xl mb-10">
+            MealDB Database
+          </h2>
+          <button className="btn btn-secondary">Pick random recipe!</button>
         </div>
       </div>
     </div>
